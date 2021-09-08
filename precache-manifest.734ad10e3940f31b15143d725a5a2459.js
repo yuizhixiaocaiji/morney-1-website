421662d0e95c7c2c058df5910d3fa3649384ce09
你çxuCheng <1390683299@qq.com>
2021-09-08 19:47:43 +0800

    deploy

---
+++ precache-manifest.734ad10e3940f31b15143d725a5a2459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a840468e22a0dc75f6",
+    "url": "/morney-1-website/css/app.b6a4bc17.css"
+  },
+  {
+    "revision": "3d4b089d390b4217e0f942bc3b61ae70",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "c2a840468e22a0dc75f6",
+    "url": "/morney-1-website/js/app.e2630ad1.js"
+  },
+  {
+    "revision": "832d4e129592a941b8bf",
+    "url": "/morney-1-website/js/chunk-vendors.7672f77f.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b98514128af3cdbb253a1f36d5908406",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
